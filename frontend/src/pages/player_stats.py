b7a9a1845f3647b6c5bf85953a89870b3b7f2f69
tledoare <tanguy.le-doare@eleve.ensai.fr>
2026-09-25 10:26:34 +0000

    amélioration

--- frontend/src/pages/player_stats.py
+++ frontend/src/pages/player_stats.py
@@ -12,6 +12,13 @@ logger = get_page_logger("list_players")
 # check_authentification()
 
 
+def ennemi(p1, p2, id_player):
+    if p1["id_player"] != int(id_player):
+        return f"{p1['username']} ({p1['elo']})"
+    else:
+        return f"{p2['username']} ({p2['elo']})"
+
+
 if st.query_params.id_player:
     joueur = api_client.get(f"/player/{st.query_params.id_player}").get("data")
     kd = api_client.get(f"/game/players/{st.query_params.id_player}/win-loss")
@@ -32,7 +39,10 @@ if st.query_params.id_player:
         gagnant = df.winner.map(
             lambda x: int(st.query_params.id_player) == x["id_player"] if x is not None else None
         )
+        df["opp"] = df.loc[:, ["player1", "player2"]].apply(
+            lambda s: ennemi(s.player1, s.player2, st.query_params.id_player), axis=1
+        )
         df.player1 = df.player1.map(lambda x: f"{x['username']} ({x['elo']})")
         df.player2 = df.player2.map(lambda x: f"{x['username']} ({x['elo']})")
         df.winner = gagnant.map(lambda x: "Win" if x else "Loss" if x is not None else "Draw")
-        st.dataframe(df.iloc[:, [1, 2, 3, 4]], hide_index=True)
+        st.dataframe(df.iloc[:, [3, 4, -1]], hide_index=True)
